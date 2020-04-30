3f51d62e5edfcd82c1e2c2f6d463ca5316085323
Mekarosi <mekarosi_assa@yahoo.com>
2020-04-30 14:38:43 +0100

    Install bootstrap and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Pet Manager's Todo</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Pet Manager's Todo</h1>
+      </div>
+    </Router>
   );
 }
 
