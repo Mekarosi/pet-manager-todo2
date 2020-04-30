0aa56c3713644a4db45d692d2a3ab13ef2b79e5d
Mekarosi <mekarosi_assa@yahoo.com>
2020-04-30 15:40:50 +0100

    Add state property and table to todo list component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
   render() {
     return (
       <div>
-        <p>Welcome to TodoList Page</p>
+        <h3>Todos List</h3>
+        <table className='table table-striped' style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Responsible</th>
+              <th>Priority</th>
+              <th>Start Date</th>
+              <th>Intended Completion Date</th>
+              <th>Edit Todo</th>
+              <th>Delete Todo</th>
+            </tr>
+          </thead>
+        </table>
       </div>
     );
   }
